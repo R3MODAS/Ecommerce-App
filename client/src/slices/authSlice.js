dcc80e7ad7661b459c3e3ef0752372df548ac2f5
Sharadindu Das <remodas7774@gmail.com>
2024-05-16 10:06:16 +0530

    configuration all done

---
+++ client/src/slices/authSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        mode: "light",
+        user: null,
+        token: null,
+        posts: []
+    },
+    reducers: {
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light"
+        },
+        setLogin: (state, action) => {
+
+        },
+        setLogout: (state) => {
+
+        },
+        setFriends: (state, action) => {
+
+        },
+        setPosts: (state, action) => {
+
+        },
+        setPost: (state, action) => {
+
+        }
+    }
+})
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions
+export default authSlice.reducer
