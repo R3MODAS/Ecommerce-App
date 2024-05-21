9c4ff90546a8172bee1ced5db90f6bc3ce049742
Sharadindu Das <remodas7774@gmail.com>
2024-05-21 15:36:39 +0530

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Profile from "./pages/Profile"
 function App() {
-
   return (
-    <BrowserRouter>
-        <Routes>
-          <Route index element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile/:userId" element={<Profile />} />
-        </Routes>
-    </BrowserRouter>
+    <div className="text-center">
+      <h1 className="text-3xl font-bold">Welcome to the Full Stack Ecommerce App</h1>
+    </div>
   )
 }
 
--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import { upload } from "../middlewares/multer.js"
-import { login, register } from "../controllers/auth.js"
-const router = Router()
-
-router.post("/register", upload.single("picture"), register)
-router.post("/login", login)
-
-export default router
