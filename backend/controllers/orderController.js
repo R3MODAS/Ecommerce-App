106a9708c792faadcb304301047f9b8c0b985bd5
Sharadindu Das <remodas7774@gmail.com>
2024-06-01 22:57:35 +0530

    backend done for now

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -60,7 +60,7 @@ exports.getSingleOrder = AsyncHandler(async (req, res, next) => {
   // check if the order exists in the db or not
   const order = await Order.findById(orderId).populate("user", "name email");
   if (!order) {
-    return new ErrorHandler("Order is not found", 400);
+    return next(new ErrorHandler("Order is not found", 400));
   }
 
   // return the response
@@ -84,5 +84,107 @@ exports.myOrders = AsyncHandler(async (req, res, next) => {
     success: true,
     message: "Got the order details successfully",
     orders,
+  });
+});
+
+// Get all orders (Admin)
+exports.getAllOrders = AsyncHandler(async (req, res, next) => {
+  // get all the orders
+  const orders = await Order.find();
+
+  // get the total amount
+  let totalAmount = 0;
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+
+  // return the response
+  return res.status(200).json({
+    success: true,
+    message: "Got all the orders successfully",
+    orders,
+    totalAmount,
+  });
+});
+
+// Update order (Admin)
+exports.updateOrder = AsyncHandler(async (req, res, next) => {
+  // get the order id
+  const orderId = req.params.id;
+
+  // get the order status from request body
+  const { status } = req.body;
+
+  // validation of data
+  if (!status) {
+    return next(new ErrorHandler("Status is required", 400));
+  }
+
+  // check if the order exists in the db or not
+  const order = await Order.findById(orderId);
+  if (!order) {
+    return next(new ErrorHandler("Order is not found", 400));
+  }
+
+  // check if the order is already delivered or not
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You have already delivered this order", 400));
+  }
+
+  // if the order status is set to shipped then update the product stock
+  if (status === "Shipped") {
+    order.orderItems.forEach(async (o) => {
+      // update the stock of the product
+      await updateStock(o.product, o.quantity);
+    });
+  }
+
+  // update the order status in db
+  order.orderStatus = status;
+
+  // set the delivered time once the order is delivered
+  if (status === "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+
+  // save the changes
+  await order.save({ validateBeforeSave: false });
+
+  // return the response
+  return res.status(200).json({
+    success: true,
+    message: "Updated the order status successfully",
+  });
+});
+
+async function updateStock(id, quantity) {
+  // find the product the user ordered
+  const product = await Product.findById(id);
+
+  console.log(product.stock);
+  // decrease the stock quantity
+  product.stock -= quantity;
+  // save the changes
+  await product.save({ validateBeforeSave: false });
+}
+
+// Delete order (Admin)
+exports.deleteOrder = AsyncHandler(async (req, res, next) => {
+  // get the order id using request params
+  const orderId = req.params.id;
+
+  // check if the order exists in the db or not
+  const order = await Order.findById(orderId);
+  if (!order) {
+    return next(new ErrorHandler("Order is not found", 400));
+  }
+
+  // remove the order
+  await order.deleteOne();
+
+  // return the response
+  return res.status(200).json({
+    success: true,
+    message: "Deleted the order successfully",
   });
 });
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -5,10 +5,20 @@ const {
   newOrder,
   getSingleOrder,
   myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
 } = require("../controllers/orderController");
 const { auth, authorizeRoles } = require("../middlewares/auth");
 
 router.route("/order/new").post(auth, newOrder);
-router.route("/order/:id").get(auth, authorizeRoles("admin"), getSingleOrder);
+router.route("/order/:id").get(auth, getSingleOrder);
 router.route("/orders/me").get(auth, myOrders);
+
+router.route("/admin/orders").get(auth, authorizeRoles("admin"), getAllOrders);
+router
+  .route("/admin/order/:id")
+  .put(auth, authorizeRoles("admin"), updateOrder)
+  .delete(auth, authorizeRoles("admin"), deleteOrder);
+
 module.exports = router;
