a6d53f8366432b3da4085a30acf74366be4b2bdd
Sharadindu Das <remodas7774@gmail.com>
2024-06-13 21:19:13 +0530

    :beers: finished with the backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -93,64 +93,98 @@ exports.myOrders = AsyncHandler(async (req, res, next) => {
 });
 
 // Get all orders (Admin)
-exports.getAllOrders = AsyncHandler(async (req,res,nex) => {
+exports.getAllOrders = AsyncHandler(async (req, res, nex) => {
   // find all the orders made by the users
-    const orders = await Order.find().populate("user", "name email").exec()
+  const orders = await Order.find().populate("user", "name email").exec()
 
-    // get the total amount
-    let totalAmount = 0
-    orders.forEach(order => {
-      totalAmount += order.totalPrice
-    })
+  // get the total amount
+  let totalAmount = 0
+  orders.forEach(order => {
+    totalAmount += order.totalPrice
+  })
 
-    // return the response
-    return res.status(200).json({
-      success: true,
-      message: "Got all the orders successfully",
-      orders,
-      totalAmount
-    })
+  // return the response
+  return res.status(200).json({
+    success: true,
+    message: "Got all the orders successfully",
+    orders,
+    totalAmount
+  })
 
 })
 
 // Update order (Admin)
-exports.updateOrder = AsyncHandler(async (req,res,next) => {
+exports.updateOrder = AsyncHandler(async (req, res, next) => {
   // get data (order status) from request body
-  const {status} = req.body
+  const { status } = req.body
 
   // get the orderId from request params
   const orderId = req.params.id
 
   // check if the order exists in the db or not
   const order = await Order.findById(orderId)
-  if(!order){
+  if (!order) {
     return next(new ErrorHandler("Order is not found", 400))
   }
 
   // check if the order is already delivered or not
-  if(order.orderStatus === "Delivered"){
+  if (order.orderStatus === "Delivered") {
     return next(new ErrorHandler("Order has been already delivered", 400))
   }
 
+  // check the order quantity and update the product stock once the product is delivered
+  order.orderItems.forEach(async (order) => {
+    await updateStock(order.product, order.quantity)
+  })
+
+  // update the order status
+  order.orderStatus = status
+
+  // update the delivery time if the order status is delivered
+  if (status === "Delivered") {
+    order.deliveredAt = Date.now()
+  }
+
+  // save all the changes
+  await order.save({ validateBeforeSave: false })
+
+  // return the response
+  return res.status(200).json({
+    success: true,
+    message: "Updated the order status successfully"
+  })
+
 })
 
+// updating the product stock once the product is delivered
+async function updateStock(productId, quantity) {
+  // find the product you want to update the stock
+  const product = await Product.findById(productId)
+
+  // update the stock once the product is delivered
+  product.stock -= quantity
+
+  // save the changes
+  await product.save({ validateBeforeSave: false })
+}
+
 // Delete order (Admin)
-exports.deleteOrder = AsyncHandler(async (req,res,next) => {
-    // get the order id from request params
-    const orderId = req.params.id
+exports.deleteOrder = AsyncHandler(async (req, res, next) => {
+  // get the order id from request params
+  const orderId = req.params.id
 
-    // check if the order exists in the db or not
-    const order = await Order.findById(orderId)
-    if(!order){
-      return next(new ErrorHandler("Order is not found", 400))
-    }
+  // check if the product exists in the db or not
+  const order = await Order.findById(orderId)
+  if (!order) {
+    return next(new ErrorHandler("Order is not found", 400))
+  }
 
-    // delete the order from db
-    await order.deleteOne()
+  // delete the order
+  await order.deleteOne()
 
-    // return the response
-    return res.status(200).json({
-      success: true,
-      message: "Order is removed successfully"
-    })
+  // return the response
+  return res.status(200).json({
+    success: true,
+    message: "Deleted the order successfully"
+  })
 })
